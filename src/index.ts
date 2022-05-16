4a9e0e5f598f534be78b440adb195808887f8c61
TheGreenBeaver <leonkhvatskin@gmail.com>
2022-05-16 04:38:55 +0300

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { useReturnToApp } from './accessControl';
-export * from './AppLink';
+export { default as AppLink } from './AppLink';
 export { default as AppRoute } from './AppRoute';
 export { default as defaultConfig } from './config';
